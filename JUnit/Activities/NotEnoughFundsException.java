adaee45dad418eec90e8917f8581495bd5985cd9
HarikrishnaReddyV <135098238+HarikrishnaReddyV@users.noreply.github.com>
2023-06-01 13:11:39 +0530

    Harikrishna Junit Activities

---
+++ JUnit/Activities/NotEnoughFundsException.java
@@ -0,0 +1,16 @@
+package Example;
+
+
+public class NotEnoughFundsException extends RuntimeException {
+
+
+
+    public NotEnoughFundsException(Integer amount, Integer balance) {
+
+        super("Attempted to withdraw " + amount + " with a balance of " + balance);
+
+    }
+
+
+
+}
